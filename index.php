9db13817e71e4c9401ec836540dd0d1fa0de0256
EnricoClem <enrico.clem94@gmail.com>
2024-05-03 15:45:38 +0200

    SETTING - generate php files and form in index

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cansura testi</title>
+</head>
+<body>
+
+    <main>
+        <h1>Censura una parola dai tuoi testi!</h1>
+
+        <form action="">
+            <p>
+                <label for="uncensored_text">Il tuo testo da censurare:</label>
+                <input type="text" name="uncensored_text" placeholder="Inserisci qui il testo.">
+            </p>
+            <p>
+                <label for="badword">La parola da rimuovere:</label>
+                <input type="text" name="badword" placeholder="Inserisci qui la parola.">
+            </p>
+            <button>CENSURA</button>
+        </form>
+    </main>
+    
+</body>
+</html>
