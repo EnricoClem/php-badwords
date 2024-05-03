f86ceb809c80c4377981e3289c0b9dac518d1761
EnricoClem <enrico.clem94@gmail.com>
2024-05-03 15:49:02 +0200

    ADD - html for response page

---
+++ server.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cansura testi</title>
+</head>
+<body>
+
+    <main>
+        <ul>
+            <li>
+                <h2>Il testo originale:</h2>
+                <p></p>
+                <h3>Lunghezza testo originale:</h3>
+                <p></p>
+            </li>
+            <li>
+                <h2>Il testo censurato:</h2>
+                <p></p>
+                <h3>Lunghezza testo censurato:</h3>
+                <p></p>
+            </li>
+        </ul>
+    </main>
+    
+</body>
+</html>
