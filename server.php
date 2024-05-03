9ea35c561435e66b08030e6bd2c017aa86806f39
EnricoClem <enrico.clem94@gmail.com>
2024-05-03 16:18:33 +0200

    ADD - bomus count of censured words

--- server.php
+++ server.php
@@ -3,7 +3,7 @@ $uncensored_text = $_GET['uncensored_text'];
 $badword = $_GET['badword'];
 $uncensored_text_lenght = strlen($uncensored_text);
 
-$censored_text = str_replace($badword, '***', $uncensored_text);
+$censored_text = str_replace($badword, '***', $uncensored_text, $count);
 $censored_text_lenght = strlen($censored_text);
 ?>
 
@@ -29,6 +29,7 @@ $censored_text_lenght = strlen($censored_text);
                 <p><?php echo $censored_text ?></p>
                 <h3>Lunghezza testo censurato:</h3>
                 <p><?php echo $censored_text_lenght ?></p>
+                <h4>Numero di censure applicate: <?php echo $count ?></h4>
             </li>
         </ul>
     </main>
